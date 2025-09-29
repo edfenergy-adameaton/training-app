8a041ea955c3e700757b2852ce10a257636a1769
My Email <my.email@edfenergy.com>
2025-09-29 11:58:46 +0100

    adding base path to images

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 const GameImages = [
   {
     name: "snake",
-    img: "/Snake.jpeg",
+    img: `${import.meta.env.BASE_URL}/Snake.jpeg`,
     url: "https://editor.p5js.org/Adsa2/full/SNx0iJ8tq",
   },
   {
     name: "aim-trainer",
-    img: "/chess.png",
+    img: `${import.meta.env.BASE_URL}/chess.png`,
     url: "https://editor.p5js.org/Adsa2/full/lrpPC4nIt",
   },
   {
     name: "flappy-bird",
-    img: "/flappy.png",
+    img: `${import.meta.env.BASE_URL}/flappy.png`,
     url: "https://editor.p5js.org/Adsa2/full/7sfQIq1yp",
   },
   {
     name: "pong",
-    img: "/pong.png",
+    img: `${import.meta.env.BASE_URL}/pong.png`,
     url: "https://editor.p5js.org/Adsa2/full/WRYvYDXT-",
   },
 ];
@@ -27,12 +27,12 @@ const GameImages = [
 const DataImages = [
   {
     name: "tennis",
-    img: "/tennis.jpg",
+    img: `${import.meta.env.BASE_URL}/tennis.jpg`,
     link: "/Database/tennis",
   },
   {
     name: "aim-trainer",
-    img: "/film.jpg",
+    img: `${import.meta.env.BASE_URL}/film.jpg`,
     link: "/Database/film",
   },
 ];
