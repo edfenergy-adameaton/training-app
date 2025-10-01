73159faf2696686bdcde037b3646d9cf441bf80d
My Email <my.email@edfenergy.com>
2025-10-01 10:21:27 +0100

    adding zipping to the provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.14.1"
     }
+    archive = {
+      source  = "hashicorp/archive" // used to make .zip or .tar files within terraform.
+      version = "~> 2.4"
+    }
+    null = {
+      source  = "hashicorp/null"    // used to run local commands within terraform
+      version = "~> 3.2"
+    }
   }
 }
 
